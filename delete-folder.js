b3d909376a1a64dd4ef57beb623e7df9c57348ff
noemiAmaya <jazzamaya13@gmail.com>
2020-04-15 16:18:21 -0500

    practice callbacks

--- delete-folder.js
+++ delete-folder.js
@@ -2,27 +2,14 @@ const fs = require('fs')
 
 const newFolder = 'folder-2'
 
-fs.readdir(newFolder, 'utf8', (error, files) => {
+
+const options = { maxRetries: 2, recursive: true }
+
+fs.rmdir(newFolder, options, (error) => {
     if (error) {
-        console.error(`La carpeta ${newFolder} no se puede leer`)
+        console.log('Error al eliminar la carpeta', error)
     }
-    console.log('El contenido de la carpeta es', files)
-
-    /*fs.unlink('',(error)=>{
-        if(error){
-            console.log('Los archivos no se pueden eliminar',error)
-            return
-        }
-        console.log ('Los archivos han sido eliminados')
-    })*/
-
-    const options = { maxRetries: 2, recursive: true }
-
-    fs.rmdir(newFolder, options, (error) => {
-        if (error) {
-            console.log('Error al eliminar la carpeta', error)
-        }
-        console.log(`La carpeta ${newFolder} fue eliminada de forma exitosa `)
-    })
-
+    console.log(`La carpeta ${newFolder} fue eliminada de forma exitosa `)
 })
+
+
